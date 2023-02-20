761a520d5cf4a9828095136827be4a97d82c420a
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2023-02-20 17:03:07 +0000

    Exibindo o formulário no consulta.html

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ class Despesa {
 
   validarDados() {
     for(let i in this) {
-      if (this[i] == undefined || this[i] == '' || this[i] == null ){
+      if (this[i] == undefined || this[i] == '' || this[i] == null){
         return false
-      } else {
-        return true
-      }
+      } 
     }
+    return true
   }
 }
+ 
 
 class Bd {
 
@@ -40,7 +40,33 @@ class Bd {
 
     localStorage.setItem('id', id)
   }
-}
+
+  recuperarTodosRegistros() {
+
+    //array de despesas
+    let despesas = Array()
+
+
+    let id = localStorage.getItem('id')
+    
+    //recuperar todas as despesas cadastradas em localStorege 
+    for(let i = 1; i <= id; i++) {
+
+      //recuperar a despesa
+      // parse = traforma em objetos literais
+      let despesa = JSON.parse(localStorage.getItem(i))
+      
+      //existe a possibilidade de haver indices que foram pulados ou removidos
+      if(despesa === null) {
+        continue
+      }
+
+      despesas.push(despesa)
+    }
+    return despesas
+  }
+
+} // fim Bd
 
 let bd = new Bd()
 
@@ -63,7 +89,7 @@ function cadastrarDespesa() {
     valor.value
   )
 
-  if (despesa.validarDados() === true) {
+  if (despesa.validarDados()) {
     bd.gravar(despesa)
     //dialog de sucesso
     $('#modalRegistraDespesa').modal('show')
@@ -73,6 +99,15 @@ function cadastrarDespesa() {
     document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
     document.getElementById('modal_voltar_corrigir').innerHTML = 'Voltar'
     document.getElementById('modal_voltar_corrigir').className = 'btn btn-success'
+
+    //limpando campo do formulario
+    ano.value = ''
+    mes.value = ''
+    dia.value = ''
+    tipo.value = ''
+    descricao.value = ''
+    valor.value = ''
+    
  
   } else {
     //dialog de erro
@@ -83,8 +118,55 @@ function cadastrarDespesa() {
     document.getElementById('modal_conteudo').innerHTML = 'Existem campos obrigatorios que não foram preenchidos'
     document.getElementById('modal_voltar_corrigir').innerHTML = 'Voltar e corrigir'
     document.getElementById('modal_voltar_corrigir').className = 'btn btn-danger'
-  }
+  } 
+} // fim de cadastrarDespesa
 
+function carregaListaDespesas() {
+    let despesas = Array()
   
+    despesas = bd.recuperarTodosRegistros()
+
+    //selecionando o elemento tbody da tabela
+    let listaDespesas =  document.getElementById('listasDespesas')
+
+    //percorrer o array despesas , listando cada despesa de forma dinâmica
+    despesas.forEach(function(d) {
+      
+      //criando a linha (tr)
+      let linha = listaDespesas.insertRow()
+
+      //criar as colunas (td)
+      linha.insertCell(0).innerHTML = `${ d.dia}/${d.mes}/${d.ano}`  
+      
+      //ajustar o tipo
+      switch (d.tipo) {
+        case '1':
+          d.tipo = 'Alimentação'
+          break;
+        case '2':
+          d.tipo = 'Educação'
+          break;
+        case '3':
+          d.tipo = 'Lazer'
+          break;
+        case '4':
+          d.tipo = 'Saúde'
+          break;
+        case '5':
+          d.tipo = 'Transporte'
+          break;
+      
+        default:
+          break;
+      }
+      linha.insertCell(1).innerHTML = d.tipo
+
+      linha.insertCell(2).innerHTML = d.descricao
+      linha.insertCell(3).innerHTML = d.valor
+
+    })
+
+
 }
+
 
